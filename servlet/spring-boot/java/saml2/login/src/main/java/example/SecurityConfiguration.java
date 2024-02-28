1f63dfb189df30e53c8979f5d952ee243a6a3746
Hongchol Sinn <hongchol.sinn@broadcom.com>
2024-02-27 16:18:39 -0800

    feature: Implement SP metadata endpoint URL with spring-security 5
    - As described at https://docs.spring.io/spring-security/reference/5.8/servlet/saml2/metadata.html.

--- servlet/spring-boot/java/saml2/login/src/main/java/example/SecurityConfiguration.java
+++ servlet/spring-boot/java/saml2/login/src/main/java/example/SecurityConfiguration.java
@@ -1,25 +1,51 @@
 package example;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.saml2.provider.service.metadata.OpenSamlMetadataResolver;
+import org.springframework.security.saml2.provider.service.registration.InMemoryRelyingPartyRegistrationRepository;
+import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistration;
+import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistrationRepository;
+import org.springframework.security.saml2.provider.service.registration.RelyingPartyRegistrations;
+import org.springframework.security.saml2.provider.service.web.DefaultRelyingPartyRegistrationResolver;
+import org.springframework.security.saml2.provider.service.web.RelyingPartyRegistrationResolver;
+import org.springframework.security.saml2.provider.service.web.Saml2MetadataFilter;
+import org.springframework.security.saml2.provider.service.web.authentication.Saml2WebSsoAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfiguration {
+    @Bean
+    RelyingPartyRegistrationRepository relyingPartyRegistrations() {
+        String assertingPartyMetadataLocation =
+                "https://dev-73893672.okta.com/app/exkef5al7mopEta1B5d7/sso/saml/metadata";
+        RelyingPartyRegistration registration = RelyingPartyRegistrations
+                .fromMetadataLocation(assertingPartyMetadataLocation)
+                .registrationId("example")
+                .build();
+        return new InMemoryRelyingPartyRegistrationRepository(registration);
+    }
 
     @Bean
-    SecurityFilterChain app(HttpSecurity http) throws Exception {
-        // @formatter:off
+    SecurityFilterChain app(HttpSecurity http,
+                            @Autowired RelyingPartyRegistrationRepository relyingPartyRegistrationRepository)
+            throws Exception {
+        RelyingPartyRegistrationResolver relyingPartyRegistrationResolver =
+                new DefaultRelyingPartyRegistrationResolver(relyingPartyRegistrationRepository);
+        Saml2MetadataFilter filter = new Saml2MetadataFilter(
+                relyingPartyRegistrationResolver,
+                new OpenSamlMetadataResolver());
+
         http
                 .authorizeHttpRequests((authorize) -> authorize
                         .anyRequest().authenticated()
                 )
                 .saml2Login(withDefaults())
-                .saml2Metadata((saml2) -> saml2.metadataUrl("/saml/metadata"));
-        // @formatter:on
+                .addFilterBefore(filter, Saml2WebSsoAuthenticationFilter.class);;
         return http.build();
     }
 }
