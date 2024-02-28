9443ff3b4c7a4a7e0e5c10f7418388de10063263
Danny Faught <danny.faught@broadcom.com>
2024-02-28 15:57:18 -0800

    feat: custom SP metadata path
    * Move metadata URL close to the one UAA uses, and the one on the main branch of the this fork. (http://localhost:8080/saml/metadata/example)
    * Not sure yet how to get "example" off of the path.
    
    [#186986697]
    
    Co-authored-by: Alicia Yingling <alicia.yingling@broadcom.com>

--- servlet/spring-boot/java/saml2/login/src/main/java/example/SecurityConfiguration.java
+++ servlet/spring-boot/java/saml2/login/src/main/java/example/SecurityConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.security.saml2.provider.service.web.RelyingPartyRegis
 import org.springframework.security.saml2.provider.service.web.Saml2MetadataFilter;
 import org.springframework.security.saml2.provider.service.web.authentication.Saml2WebSsoAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -39,6 +40,7 @@ public class SecurityConfiguration {
         Saml2MetadataFilter filter = new Saml2MetadataFilter(
                 relyingPartyRegistrationResolver,
                 new OpenSamlMetadataResolver());
+        filter.setRequestMatcher(new AntPathRequestMatcher("/saml/metadata/{registrationId}", "GET"));
 
         http
                 .authorizeHttpRequests((authorize) -> authorize
